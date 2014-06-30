437df7c1d489b038275e7c8537676ffac8b5a82f
Dipesh Patel <dip2112@gmail.com>
2014-06-30 02:39:03 +0000

    0400

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -1,5 +1,5 @@
 module.exports.formLetter = function(firstName, senderName, message) {
-
+return "Hello" + firstName + message + "Sincerly," + senderName
 };
 
 module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) {
