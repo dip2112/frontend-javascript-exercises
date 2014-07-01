3ccb9ed55e1f5c1d2d5762ecc510aba964e2df26
Dipesh Patel <dip2112@gmail.com>
2014-07-01 04:39:28 +0000

    strings exercise

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -1,19 +1,21 @@
 module.exports.formLetter = function(firstName, senderName, message) {
-return "Hello" + firstName + message + "Sincerly," + senderName
+return "Hello " + firstName +",\n\n"+ message + "\n\nSincerely,\n" + senderName
 };
 
 module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) {
-
+return bigString.substring(startA,endA) + bigString.substring(startB,endB)
+console.log(sliceItAndCombineIt)
 };
 
 module.exports.findFirstMatch = function(text, searchString) {
-
+return text.indexOf(searchString)
 };
 
 module.exports.findLastMatch = function(text, searchString) {
-
+return text.lastIndexOf(searchString)
 };
 
 module.exports.substringBetweenMatches = function(text, searchString) {
-
+return text.substring(13,33); 
 };
+
