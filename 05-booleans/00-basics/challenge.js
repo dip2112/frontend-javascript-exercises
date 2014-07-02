e7b79e5279f0fe9376a4b1c8434d06e4cd198358
Dipesh Patel <dip2112@gmail.com>
2014-07-02 01:32:18 +0000

    boolean exercise v1

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,19 +1,20 @@
 module.exports.equalStrings = function(stringOne, stringTwo) {
-
+return stringOne == stringTwo
 };
 
 module.exports.notEqual = function(one, two) {
-
+return one !== two
 };
 
 module.exports.inBetween = function(lower, middle, upper) {
-
+return lower < middle && middle < upper
 };
 
 module.exports.outsideRanges = function(number) {
-
+return number !== ((1 < number && number < 6) || (10 < number && number < 20) || (42 < number && number < 75) || 10 || 20 || 75)
+//Ordering Problem???//
 };
 
 module.exports.nameAndPrice = function(name, price) {
-
+return (name === 'NYTimes' || 'LATimes') && (price >= 1)
 };
