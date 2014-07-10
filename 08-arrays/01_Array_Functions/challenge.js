f4cd76424a86287123a4059b71585e856829f7e2
Dipesh Patel <dip2112@gmail.com>
2014-07-10 02:53:26 +0000

    Arrays ex

--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,5 +1,23 @@
-module.exports.reversePlusOne = undefined;
+module.exports.reversePlusOne = function(x){
+x.reverse();
+x.unshift(1);
+return x
+};
 
-module.exports.plusesEverywhere = undefined;
 
-module.exports.arrayQuantityPlusOne = undefined;
+/*
+module.exports.reversePlusOne = function(x){
+([x,x]).reverse().unshift(1);
+return x;
+};
+*/
+
+module.exports.plusesEverywhere = function(x){
+return x.join("+");
+};
+
+/why not x.join("+") then return x/
+
+module.exports.arrayQuantityPlusOne = function(x){
+return x.length + 1;
+};
